a08a4c269cf7596d58124482974cfdee1a10086d
sweta <swetaarawal972@gmail.com>
2025-01-11 23:16:33 +0530

    file added

---
+++ Level-1/AverageMarks.java
@@ -0,0 +1,8 @@
+public class AverageMarks{
+    public static void main(String[] args){
+        int maths= 94;
+        int physics=95;
+        int chemistry=96;
+        System.out.println("Sam's average mark in PCM is " + (physics+chemistry+maths)/3);
+    }
+ }
