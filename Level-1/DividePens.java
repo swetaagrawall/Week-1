f3c829d57035925018920cc1969d49bae99e993d
sweta <swetaarawal972@gmail.com>
2025-01-11 23:17:05 +0530

    file added

---
+++ Level-1/DividePens.java
@@ -0,0 +1,12 @@
+public class DividePens{
+    public static void main(String[] args){
+      
+        int pens=14;
+       //calculating penPerstudent and remaining pens
+        int penPerstudent= 14/3;
+        int remainingPens=14%3;
+       //Printing output to terminal
+        System.out.println("The Pen per student is : "+(penPerstudent)+" and the remaining pen not distributes is : " + (remainingPens));
+      
+    }
+ }
