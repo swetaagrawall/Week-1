1b6b1fbdf96b1d735d4fdde1e51a68c5454dd96b
sweta <swetaarawal972@gmail.com>
2025-01-11 23:17:18 +0530

    file added

---
+++ Level-1/HarryAge.java
@@ -0,0 +1,7 @@
+public class HarryAge{
+    public static void main(String[] args){
+        int currentYear= 2024;
+        int birthYear=2000;
+        System.out.println("Harry's age in 2024 : " + (currentYear-birthYear));
+    }
+ }
