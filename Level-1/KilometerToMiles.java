670969de165142227a416cad10854d043adc0333
sweta <swetaarawal972@gmail.com>
2025-01-11 23:17:39 +0530

    file added

---
+++ Level-1/KilometerToMiles.java
@@ -0,0 +1,7 @@
+public class KilometerToMiles{
+    public static void main(String[] args){
+       //Taking distance in kilometers
+        double distanceInKm= 10.8;
+      //converting it into miles
+        System.out.println("The distance "+(distanceInKm) +" in miles is "+ (distanceInKm*1.6));
+    }
